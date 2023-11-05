78342abbe4391768725512aeb372b341155fe58e
Omar Dembele <omardembele820@gmail.com>
2023-11-05 16:03:56 +0000

    commit

--- src/main/java/com/esmt/memoire_back2023/controller/HospitalisationController.java
+++ src/main/java/com/esmt/memoire_back2023/controller/HospitalisationController.java
@@ -1,13 +1,9 @@
 package com.esmt.memoire_back2023.controller;
 
 import com.esmt.memoire_back2023.dto.HospitalisationDTO;
-import com.esmt.memoire_back2023.entity.Consultation;
 import com.esmt.memoire_back2023.entity.Hospitalisation;
-import com.esmt.memoire_back2023.repository.DossierRepository;
-import com.esmt.memoire_back2023.repository.HospitalisationRepository;
 import com.esmt.memoire_back2023.services.HospitalisationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -41,10 +37,25 @@ public class HospitalisationController {
 
     }
 
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Hospitalisation> updateHospi(@PathVariable Long id, @RequestBody HospitalisationDTO hospitalisationDTO) {
+        Hospitalisation updateHospi = hospitalisationService.updateHospi(id, hospitalisationDTO);
+        if (updateHospi != null) {
+            return ResponseEntity.ok(updateHospi);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @GetMapping("/dossier/{dossierMedicalId}")
     public List<Hospitalisation> getConsultationsByDossierMedical(@PathVariable Long dossierMedicalId) {
         List<Hospitalisation> hospitalisations = hospitalisationService.getHospitalisationsByDossierMedical(dossierMedicalId);
         return hospitalisations;
+    }
+
+    @GetMapping(path = "/{id}")
+    public Hospitalisation getConsultationById(@PathVariable Long id) {
+        return hospitalisationService.getHospitalisationById(id);
     }
 
 }
--- src/main/java/com/esmt/memoire_back2023/services/HospitalisationService.java
+++ src/main/java/com/esmt/memoire_back2023/services/HospitalisationService.java
@@ -1,6 +1,7 @@
 package com.esmt.memoire_back2023.services;
 
 import com.esmt.memoire_back2023.dto.HospitalisationDTO;
+import com.esmt.memoire_back2023.dto.PatientDTO;
 import com.esmt.memoire_back2023.entity.Consultation;
 import com.esmt.memoire_back2023.entity.Hospitalisation;
 import com.esmt.memoire_back2023.entity.Patient;
@@ -17,7 +18,9 @@ public interface HospitalisationService {
 
     List<Hospitalisation> getAllHospi();
 
-    HospitalisationDTO getHospitalisationById(Long id);
+    Hospitalisation updateHospi(Long id, HospitalisationDTO hospitalisationDTO);
+
+    Hospitalisation getHospitalisationById(Long id);
 
     List<Hospitalisation> getHospitalisationsByDossierMedical(Long dossierMedicalId);
 
